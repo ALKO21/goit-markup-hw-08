dc6f964a9b49e05dd94405ab6878085c0a41b8a4
Aleksandr Koval (AL KO) <alkorace@gmail.com>
2022-05-13 21:34:34 +0300

    Исправил ошибки

--- js/body-hidden.js
+++ js/body-hidden.js
@@ -1,15 +1,31 @@
 
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector("[body-hidden-active]"),
+//     closeModalBtn: document.querySelector("[body-hidden-disactive]"),
+//     modal: document.querySelector("[body-hidden]"),
+//   };
+
+//   refs.openModalBtn.addEventListener("click", toggleModal);
+//   refs.closeModalBtn.addEventListener("click", toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle("body-hidden");
+//   }
+// })();
+
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector("[body-hidden-active]"),
-    closeModalBtn: document.querySelector("[body-hidden-disactive]"),
-    modal: document.querySelector("[body-hidden]"),
-  };
+  const menuBtnRef = document.querySelector("[body-hidden-active]");
+  const mobileMenuRef = document.querySelector("[body-hidden]");
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  menuBtnRef.addEventListener("click", () => {
+      // const expanded =
+      //     menuBtnRef.getAttribute("aria-expanded") === true || false;
+      
+      // menuBtnRef.classList.toggle("body-hidden");
+      // menuBtnRef.setAttribute("aria-expanded", !expanded);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("body-hidden");
-  }
+      mobileMenuRef .classList .toggle("body-hidden");
+  
+  });
 })();
